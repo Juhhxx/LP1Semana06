e7dd72e969dd29824f2aa6bf0c388ac2cf9319ab
Julia Costa <juliacosta232005@gmail.com>
2024-04-12 00:31:29 +0100

    Add Constructor for class Color

---
+++ ColorSpheres/Color.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        private byte _red;
+        private byte _green;
+        private byte _blue;
+        private byte _alpha;
+        Color(byte _red, byte _green, byte _blue, byte _alpha)
+        {
+            this._red = _red;
+            this._green = _green;
+            this._blue = _blue;
+            this._alpha= _alpha;
+        }
+    }
+}
