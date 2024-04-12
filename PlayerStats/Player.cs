a14413a501b31e129246dc69121a5be91cfbaf18
Julia Costa <juliacosta232005@gmail.com>
2024-04-12 02:47:49 +0100

    Add Player class and instance variables

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlayerStats
+{
+    public class Player
+    {
+        private float _highScore;
+        private int _playedGames;
+        private int _wonGames;
+    }
+}
