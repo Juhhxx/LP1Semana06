3af233eccdaea6ba98bcce2d007dd168d214843a
Juhhxx <juliacosta232005@gmail.com>
2024-04-12 01:16:17 +0100

    Add Constructor to class Sphere

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -4,6 +4,14 @@ namespace ColorSpheres
 {
     public class Sphere
     {
-       
+        private Color _color;
+        private int _radius;
+        private int _throw;
+        Sphere(Color _color, int _radius)
+        {
+            this._color = _color;
+            this._radius = _radius;
+            _throw = 0;
+        }
     }
 }
