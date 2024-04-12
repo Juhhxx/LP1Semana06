4c8357921f54d684d55638cae8596b89a51f2872
Julia Costa <juliacosta232005@gmail.com>
2024-04-12 02:38:52 +0100

    Change code to fit new proprety usage

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -23,22 +23,22 @@ namespace BetterColorSpheres
                     ball2.Throw();
                 }
             }
-            Console.WriteLine($"Ball1 throws: {ball1.GetTimesThrown()}");
-            Console.WriteLine($"Ball2 throws: {ball2.GetTimesThrown()}");
+            Console.WriteLine($"Ball1 throws: {ball1.Thrown}");
+            Console.WriteLine($"Ball2 throws: {ball2.Thrown}");
             ball2.Pop();
             ball1.Throw();
             ball2.Throw();
-            Console.WriteLine($"Ball1 throws: {ball1.GetTimesThrown()}");
-            Console.WriteLine($"Ball2 throws: {ball2.GetTimesThrown()}");
+            Console.WriteLine($"Ball1 throws: {ball1.Thrown}");
+            Console.WriteLine($"Ball2 throws: {ball2.Thrown}");
 
             Sphere[] balls = new Sphere[2] {ball1,ball2};
 
             for (int i = 0; i < 2; i ++)
             {
-                byte r = balls[i].GetColor().GetRed();
-                byte g = balls[i].GetColor().GetGreen();
-                byte b = balls[i].GetColor().GetBlue();
-                byte a = balls[i].GetColor().GetAlpha();
+                byte r = balls[i].Color.Red;
+                byte g = balls[i].Color.Green;
+                byte b = balls[i].Color.Blue;
+                byte a = balls[i].Color.Alpha;
 
                 Console.WriteLine($"Ball{i + 1} color: ({r}, {g}, {b}, {a})");
             }
